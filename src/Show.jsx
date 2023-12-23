c90bf9ad96315e78b281836206614b97f2d5841e
gjaegyun <jaegyungim06@gmail.com>
2023-12-24 04:05:05 +0900

    Change StyleFile

--- src/Show.jsx
+++ src/Show.jsx
@@ -14,68 +14,6 @@ import ConIcon2 from './svg/ConIcon2';
 import ConIcon3 from './svg/ConIcon3';
 import ConIcon4 from './svg/ConIcon4';
 
-import styled from '@emotion/styled';
-
-
-const StyledButton = styled.button`
-    --font-size: 16px;
-    --duration: 0.44s;
-    --move-hover: -4px;
-
-    padding: 16px 32px;
-    font-family: 'Roboto';
-    font-weight: 500;
-    line-height: var(--font-size);
-    border-radius: 24px;
-    display: block;
-    outline: none;
-    appearance: none;
-    border: none;
-    text-decoration: none;
-    font-size: var(--font-size);
-    letter-spacing: 0.5px;
-    display: flex;
-    overflow: hidden;
-    border: #8bde22;
-    background: #fff;
-    color: #8bde22;
-    box-shadow: 0 2px 8px -1px #729a3e;
-    transform: translateY(var(--y)) translateZ(0);
-    transition: transform var(--duration) ease, box-shadow var(--duration) ease;
-
-    &:hover {
-        --y: var(--move-hover);
-        box-shadow: 0 4px 20px -2px rgba(39, 94, 254, 0.5);
-        span {
-        --m: calc(var(--font-size) * -1);
-        }
-    }
-
-    div {
-        display: flex;
-        overflow: hidden;
-        text-shadow: 0 var(--font-shadow) 0 var(--text);
-        span {
-        display: block;
-        backface-visibility: hidden;
-        font-style: normal;
-        transition: transform var(--duration) ease;
-
-        &:nth-child(odd) {
-            --font-shadow: var(--font-size);
-        }
-
-        &:nth-child(even) {
-            --font-shadow: calc(var(--font-size) * -1);
-        }
-
-        &:nth-child(n) {
-            --m: calc(var(--font-size) * -1);
-        }
-        }
-    }
-`;
-
 function Show() {
     const navigate = useNavigate();
     const [scrollY, setScrollY] = useState(0);
@@ -102,7 +40,7 @@ function Show() {
 
     const scale = Math.min(1 + scrollY / 20, maxScale);
     const opacity = Math.max(0, 1 - (scrollY - maxScale * 10) / fadeThreshold);
-    const translateX = Math.max(0, (scrollY - secondContainerTrigger * 8) / 7);
+    const translateX = Math.max(0, (scrollY - secondContainerTrigger * 10) / 6);
 
     const MainBtn = () => {
         navigate(`/con`);
@@ -111,15 +49,7 @@ function Show() {
     return (
         <S.StyledBody>
             <S.Body>
-                <S.ConIcon 
-                    style={{
-                        transform: `scale(${scale})`,
-                        position: 'fixed',
-                        top: '45%',
-                        zIndex: '999',
-                        opacity: opacity,
-                    }}
-                >
+                <S.ConIcon1 style={{ transform: `scale(${scale})`, opacity: opacity, }}>
                     <ConIcon1 />
                     <S.IncludeIcon3>
                         <ConIcon2 />
@@ -128,15 +58,9 @@ function Show() {
                         </S.Just>
                     </S.IncludeIcon3>
                     <ConIcon4 />
-                </S.ConIcon>
-                <div 
-                    style={{
-                        position: 'relative',
-                        top: '112rem',
-                        right: '10rem',
-                        transform: `translateX(${translateX}px)`,
-                    }}
-                >
+                </S.ConIcon1>
+
+                <S.LeftRelative1 style={{ transform: `translateX(${translateX}px)` }}>
                     <S.InfoContainer>
                         <S.ImgContainer 
                             src={reactIcon} 
@@ -145,22 +69,14 @@ function Show() {
                         <S.TextContainer>
                             FRONT-END
                         </S.TextContainer>
-                        
                     </S.InfoContainer>
                     <br />
-                        <S.ServTextContainer>
-                            담당 : 김재균(더모먼트)
-                        </S.ServTextContainer>
-                </div>
+                    <S.ServTextContainer>
+                        담당 : 김재균(더모먼트)
+                    </S.ServTextContainer>
+                </S.LeftRelative1>
 
-                <div 
-                    style={{
-                        position: 'relative',
-                        top: '132rem',
-                        left: '10rem',
-                        transform: `translateX(${-translateX}px)`,
-                    }}
-                >
+                <S.RightRelative1 style={{ transform: `translateX(${-translateX}px)` }}>
                     <S.InfoContainer>
                         <S.ImgContainer 
                             src={springIcon} 
@@ -169,22 +85,14 @@ function Show() {
                         <S.TextContainer>
                             BACK-END
                         </S.TextContainer>
-                        
                     </S.InfoContainer>
                     <br />
-                        <S.ServTextContainer>
-                            담당 : 엄지성(MSG)
-                        </S.ServTextContainer>
-                </div>
+                    <S.ServTextContainer>
+                        담당 : 엄지성(MSG)
+                    </S.ServTextContainer>
+                </S.RightRelative1>
 
-                <div 
-                    style={{
-                        position: 'relative',
-                        top: '152rem',
-                        right: '10rem',
-                        transform: `translateX(${translateX}px)`,
-                    }}
-                >
+                <S.LeftRelative2 style={{ transform: `translateX(${translateX}px)` }}>
                     <S.InfoContainer>
                         <S.ImgContainer 
                             src={rasberryPiIcon} 
@@ -193,22 +101,14 @@ function Show() {
                         <S.TextContainer>
                             IoT-ENG
                         </S.TextContainer>
-                        
                     </S.InfoContainer>
                     <br />
-                        <S.ServTextContainer>
-                            담당 : 이진헌, 윤상혁(기능반)
-                        </S.ServTextContainer>
-                </div>
+                    <S.ServTextContainer>
+                        담당 : 이진헌, 윤상혁(기능반)
+                    </S.ServTextContainer>
+                </S.LeftRelative2>
 
-                <div 
-                    style={{
-                        position: 'relative',
-                        top: '172rem',
-                        left: '10rem',
-                        transform: `translateX(${-translateX}px)`,
-                    }}
-                >
+                <S.RightRelative2 style={{ transform: `translateX(${-translateX}px)` }}>
                     <S.InfoContainer>
                         <S.ImgContainer 
                             src={figmaIcon} 
@@ -217,28 +117,17 @@ function Show() {
                         <S.TextContainer>
                             DESIGNER
                         </S.TextContainer>
-                        
                     </S.InfoContainer>
                     <br />
-                        <S.ServTextContainer>
-                            담당 : 오은찬(잡탕)
-                        </S.ServTextContainer>
-                </div>
+                    <S.ServTextContainer>
+                        담당 : 오은찬(잡탕)
+                    </S.ServTextContainer>
+                </S.RightRelative2>
 
-                <div 
-                    style={{
-                        position: 'relative',
-                        top: '202rem',
-                        bottom: '20rem',
-                        transform: `scale(${contributorsScale})`,
-                    }}
-                >
-                    <S.InfoContainer  style={{ marginBottom: '20rem', flexDirection: "column", marginBottom: '20rem'}}>
-                        <S.TextContainer style={{gap: '3.5rem', fontSize: '5rem'}}>
-                            <S.ConIcon 
-                                style={{
-                                    transform: `scale(2.42)`,
-                                }}>
+                <S.Contributors style={{ transform: `scale(${contributorsScale})` }}>
+                    <S.InfoContainer alone={'alone'}>
+                        <S.TextContainer alone={'alone'}>
+                            <S.ConIcon2 style={{ transform: `scale(2.42)` }}>
                                 <ConIcon1 />
                                 <S.IncludeIcon3>
                                     <ConIcon2 />
@@ -247,20 +136,17 @@ function Show() {
                                     </S.Just>
                                 </S.IncludeIcon3>
                                 <ConIcon4 />
-                            </S.ConIcon>
+                            </S.ConIcon2>
                             TRIBUTORS
                         </S.TextContainer>
-
                         <br />
                         <S.ServTextContainer>
                             신희성, 방가온, 장예슬, 정상혁, BSC
                         </S.ServTextContainer>
-                        
                     </S.InfoContainer>
-                    
-                </div>
+                </S.Contributors>
                 <S.ButtonWrapper>
-                    <StyledButton onClick={MainBtn}>바로가기</StyledButton>
+                    <S.StyledButton onClick={MainBtn}>바로가기</S.StyledButton>
                 </S.ButtonWrapper>
             </S.Body>
         </S.StyledBody>
--- src/ShowStyle.jsx
+++ src/ShowStyle.jsx
@@ -16,7 +16,19 @@ export const Body = styled.div`
     gap: 10rem;
 `;
 
-export const ConIcon = styled.div`
+export const ConIcon1 = styled.div`
+    display: flex;
+    align-items: flex-start;
+    gap: 0.125rem;
+    justify-content: center;
+    transition: transform 0.5s ease-in-out; 
+    transform-origin: center center; 
+    position: fixed;
+    top: 45%;
+    z-index: 999;
+`;
+
+export const ConIcon2 = styled.div`
     display: flex;
     align-items: flex-start;
     gap: 0.125rem;
@@ -52,7 +64,8 @@ export const InfoContainer = styled.div`
     justify-content: center;
     align-items: center;
     gap: 1rem;
-    
+    margin-bottom: ${(props) => (props.alone ? '20rem' : '')};
+    flex-direction: ${(props) => (props.alone ? 'column' : '')};
 `
 
 export const ImgContainer = styled.img`
@@ -62,11 +75,12 @@ export const ImgContainer = styled.img`
 `
 
 export const TextContainer = styled.p`
-    font-size: 4rem;
     font-weight: 700;
     margin: 0;
     display: flex;
     align-items: center;
+    gap: ${(props) => (props.alone ? '3.5rem' : '')};
+    font-size: ${(props) => (props.alone ? '5rem' : '4rem')};
 `
 
 export const ServTextContainer = styled.p`
@@ -74,4 +88,92 @@ export const ServTextContainer = styled.p`
     color: gray;
     font-weight: 900;
     margin: 0;
+`
+
+export const StyledButton = styled.button`
+    --font-size: 16px;
+    --duration: 0.44s;
+    --move-hover: -4px;
+
+    padding: 16px 32px;
+    font-family: 'Roboto';
+    font-weight: 500;
+    line-height: var(--font-size);
+    border-radius: 24px;
+    display: block;
+    outline: none;
+    appearance: none;
+    border: none;
+    text-decoration: none;
+    font-size: var(--font-size);
+    letter-spacing: 0.5px;
+    display: flex;
+    overflow: hidden;
+    border: #8bde22;
+    background: #fff;
+    color: #8bde22;
+    box-shadow: 0 2px 8px -1px #729a3e;
+    transform: translateY(var(--y)) translateZ(0);
+    transition: transform var(--duration) ease, box-shadow var(--duration) ease;
+
+    &:hover {
+        --y: var(--move-hover);
+        box-shadow: 0 4px 20px -2px rgba(39, 94, 254, 0.5);
+        span {
+        --m: calc(var(--font-size) * -1);
+        }
+    }
+
+    div {
+        display: flex;
+        overflow: hidden;
+        text-shadow: 0 var(--font-shadow) 0 var(--text);
+        span {
+        display: block;
+        backface-visibility: hidden;
+        font-style: normal;
+        transition: transform var(--duration) ease;
+
+        &:nth-child(odd) {
+            --font-shadow: var(--font-size);
+        }
+
+        &:nth-child(even) {
+            --font-shadow: calc(var(--font-size) * -1);
+        }
+
+        &:nth-child(n) {
+            --m: calc(var(--font-size) * -1);
+        }
+        }
+    }
+`;
+
+export const LeftRelative1 = styled.div`
+    position: relative;
+    top: 112rem;
+    right: 10rem;
+`
+
+export const LeftRelative2 = styled.div`
+    position: relative;
+    top: 152rem;
+    right: 10rem;
+`
+
+export const RightRelative1 = styled.div`
+    position: relative;
+    top: 132rem;
+    left: 10rem;
+`
+
+export const RightRelative2 = styled.div`
+    position: relative;
+    top: 172rem;
+    left: 10rem;
+`
+
+export const Contributors = styled.div`
+    position: relative;
+    top: 202rem;
 `
